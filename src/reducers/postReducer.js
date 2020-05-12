71f900019039414b15d0f18d23d33d94f8695f53
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-05-12 09:00:48 -0700

    post reducer test is passing

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,7 +1,23 @@
+import { addEntry } from '../actions/postActions';
+import reducer from './postReducer.test';
+
 describe('entry reducer', () => {
     it('handles the ADD_ENTRY action', () => {
         const state = {
-            entry: 
-        }
-    });
-});
+            blog: {
+                title: 'body of entry'
+            }
+        };
+
+        const post = addEntry('title','body of entry');
+        
+        const newState = reducer(state, post);
+
+        expect(newState).toEqual({
+            blog: {
+                title: 'body of entry',
+                title: 'body of entry'
+            }
+        });
+     });
+ });
